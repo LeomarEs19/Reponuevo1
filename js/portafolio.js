74a957b64a9c601070ec779305de731c687b49f2
LeomarEs19 <lespanacastillo20@gmail.com>
2023-05-26 22:20:39 -0500

    nuevooo

--- js/portafolio.js
+++ js/portafolio.js
@@ -1,6 +1,15 @@
 
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+
 let sections = document.querySelectorAll('section');
-let sections = document.querySelectorAll('header nav a');
+let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
     sections.forEach(sec => {
@@ -12,12 +21,21 @@ window.onscroll = () => {
         if(top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a [href*=' + id + ']').classList.add('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
+            
+            sec.classList.add('showanimate');
+        }
+        
+        else {
+            sec.classList.remove('showanimate');
         }
     });
 
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
